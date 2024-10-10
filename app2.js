3f5e02857f7528266f2775d2da3d28908d47736e
syed Anees <167554933+SyedAnees55@users.noreply.github.com>
2024-10-11 00:23:33 +0500

    Add files via upload

--- app2.js
+++ app2.js
@@ -4,25 +4,25 @@ button.addEventListener('click', function(){
         // tableform.textContent =`Your Grad is A ${table}`
         for(let count=1; count<=10; count++){
               if (count ==1 ) {
-                  tableform1.textContent =`5 x = ${table * 1}`
+                  tableform1.textContent =`${table} x = ${table * 1}`
               }else if (count == 2) {
-                    tableform2.textContent =`5 x = ${table * 2}`
+                    tableform2.textContent =`${table} x = ${table * 2}`
               }else if (count == 3) {
-                    tableform3.textContent =`5 x = ${table * 3}`
+                    tableform3.textContent =`${table} x = ${table * 3}`
               }else if (count == 4) {
-                    tableform4.textContent =`5 x = ${table * 4}`
+                    tableform4.textContent =`${table} x = ${table * 4}`
               }else if (count == 5) {
-                    tableform5.textContent =`5 x = ${table * 5}`
+                    tableform5.textContent =`${table} x = ${table * 5}`
               }else if (count == 6) {
-                    tableform6.textContent =`5 x = ${table * 6}`
+                    tableform6.textContent =`${table} x = ${table * 6}`
               }else if (count == 7) {
-                    tableform7.textContent =`5 x = ${table * 7}`
+                    tableform7.textContent =`${table} x = ${table * 7}`
               }else if (count == 8) {
-                    tableform8.textContent =`5 x = ${table * 8}`
+                    tableform8.textContent =`${table} x = ${table * 8}`
               }else if (count == 9) {
-                    tableform9.textContent =`5 x = ${table * 9}`
+                    tableform9.textContent =`${table} x = ${table * 9}`
               }else if (count == 10) {
-                    tableform0.textContent =`5 x = ${table * 10}`
+                    tableform0.textContent =`${table} x = ${table * 10}`
               }
               
         }
